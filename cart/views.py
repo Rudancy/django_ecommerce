6db29d1330cd9666cc1ff1bacaac746f62e7a9f6
Ubuntu <ubuntu@ip-172-31-92-241.ec2.internal>
2020-05-17 14:02:45 +0000

    search app added

--- cart/views.py
+++ cart/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect, reverse
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def view_cart(request):
     """A View that renders the cart contents page"""
     return render(request, "cart.html")
 
-
+@login_required
 def add_to_cart(request, id):
     """Add a quantity of the specified product to the cart"""
     quantity = int(request.POST.get('quantity'))
@@ -14,8 +15,9 @@ def add_to_cart(request, id):
     if id in cart:
         cart[id] = int(cart[id]) + quantity      
     else:
-        cart[id] = cart.get(id, quantity) 
-
+        cart[id] = cart.get(id, quantity)
+    
+    
     request.session['cart'] = cart
     return redirect(reverse('index'))
 
@@ -30,8 +32,6 @@ def adjust_cart(request, id):
 
     if quantity > 0:
         cart[id] = quantity
-        #if cart[id] is cart[id] in cart:
-            #quantity 
     else:
         cart.pop(id)
     
