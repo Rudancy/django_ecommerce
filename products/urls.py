4e705d0aea3d57b6ab1eefe0aa21341bfe300d39
Ubuntu <ubuntu@ip-172-31-92-241.ec2.internal>
2020-05-25 18:27:27 +0000

    added more products

--- products/urls.py
+++ products/urls.py
@@ -4,7 +4,7 @@ from .views import all_products, product_information, brand_products, age_produc
 
 urlpatterns=[
     url(r'^$', all_products, name='products'),
-    url(r'^information/(?P<id>\d+)', product_information, name='product_information'),
+    url(r'^information/(?P<pk>\d+)', product_information, name='product_information'),
     url(r'^age/(?P<AGE_CHOICES>\w+)', age_products, name='age_products'),
     url(r'^brand/(?P<BRAND_CHOICES>\w+)', brand_products, name='brand_products'),
     ]
