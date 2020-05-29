625722a4aaa6edfc330fc21a96fba6b4c1975e8a
Ubuntu <ubuntu@ip-172-31-92-241.ec2.internal>
2020-05-29 14:03:40 +0000

    carousel added, and new images

--- home/views.py
+++ home/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from product.models import Products
 
-# Create your views here.
+
+
+
+def index(request):
+    """A view displays the index page"""
+
+    # To find the first three ordered by the lowest price: 
+    Product = Products.objects.all()
+
+    return render(request, "index.html", {"Product": Product})
